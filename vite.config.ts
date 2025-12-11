6924d15c458dab8e76736120d66349b657ebdaf3
Chosen-Coffee <chosen.coffee.boutique@gmail.com>
2025-12-10 23:43:03 -0500

    fix: typo base url on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ChosenCoffeeWebsite/",   // <-- NECESARIO PARA GITHUB PAGES
+  base: "/ChosenCoffeeWebSite/",   // <-- NECESARIO PARA GITHUB PAGES
   server: {
     host: "::",
     port: 8080,
